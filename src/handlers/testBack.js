252df3ec9886f313e1fa1d70b1a1dadc89687a86
omar-elsayad <kklskull1234@gmail.com>
2023-02-20 20:41:07 +0200

    test

--- src/handlers/testBack.js
+++ src/handlers/testBack.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getHandler = (path) => {
     return new Promise ((resolved, rejected) => {
         axios({
-            url: `${path}`,
+            url: `https://gym-backend-nine.vercel.app${path}`,
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
